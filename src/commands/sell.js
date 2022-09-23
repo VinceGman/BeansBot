ad980453ddcd3b20614004f02770906d86e6194e
Vince Gonzalez <seventito777@gmail.com>
2022-09-22 21:23:19 -0700

    sell guide and bugfix

--- src/commands/sell.js
+++ src/commands/sell.js
@@ -18,6 +18,19 @@ module.exports = {
     admin: false,
     type: "production",
     async execute(discord_client, msg, args, admin) {
+        if (args.length == 0) {
+            let sell_guide = new MessageEmbed()
+                .setTitle(`Sell Guide`)
+                .setDescription('To ensure you sell the product you intend, you now must specify rank at listing.')
+                .setColor('#000000')
+                .addField('+sell Nezuko Kamado 123 10000', `lists card 'Nezuko Kamado' with rank #123 at 10000 credits on the market`, false)
+                .setFooter({ text: `${msg.author.username}#${msg.author.discriminator}` })
+                .setTimestamp();
+
+            msg.channel.send({ embeds: [sell_guide] });
+            return;
+        }
+
         let current_time = Math.floor(Date.now() / 1000);
         if (timer.hasOwnProperty(msg.author.id.toString())) {
             if (current_time < timer[msg.author.id.toString()] + cooldown) {
@@ -32,14 +45,17 @@ module.exports = {
             msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - +sell <name> <price>`);
             return;
         }
-
-        let character = await db.collection(`cards`).where('owner_id', '==', msg.author.id).where('name', '==', args.join(' ')).get();
-        if (character._docs()[0] == null) {
-            msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - Character not found.`);
+        else if (price < 0) {
+            msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - +sell <name> <price>`);
             return;
         }
 
-        // console.log(character._docs()[0]._fieldsProto, price);
+        let rank = args.pop();
+        let character = await db.collection(`cards`).where('owner_id', '==', msg.author.id).where('name', '==', args.join(' ')).where('rank', '==', rank).get();
+        if (character._docs()[0] == null) {
+            msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - Character not found. +sell <name> <rank> <price>`);
+            return;
+        }
 
         const res = await db.collection('cards').doc(`${character._docs()[0]._ref._path.segments[1]}`).update({
             for_sale: true,
