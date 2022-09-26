d81adc2fd0339c0e586f6d9b086dc5492f5ab7ae
Vince Gonzalez <seventito777@gmail.com>
2022-09-26 13:06:18 -0700

    index bug

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ discord_client.on('messageCreate', async msg => {
     const args = msg.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (msg.channel.name != 'commands' && !(msg.member.roles.cache.some(role => role.name === 'Mods') && command != 'topic')) return;
+    if (msg.channel.name != 'commands' && !(msg.member.roles.cache.some(role => role.name === 'Mods') && command.name != 'topic')) return;
 
     try {
       if (discord_client.commands.get(command).type != run_type) return;
