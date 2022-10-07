e97aff019ce41fe469b863b8a7e99e9e1d43b1fb
Vince Gonzalez <seventito777@gmail.com>
2022-10-06 22:38:50 -0700

    command use restrictions

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ discord_client.on('messageCreate', async msg => {
     const args = msg.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    // if (!msg.member.roles.cache.some(role => role.name === 'Mods') && msg.channel.name != 'commands' && command.name != 'topic') return;
+    if (msg.channel.name != 'commands' && command != 'topic' && msg.author.id != '183019001058689025') return; // owner : (await msg.guild.fetchOwner()).user.id
 
     try {
       if (discord_client.commands.get(command).type != run_type) return;
