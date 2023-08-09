b5410eb5ae2c3d93f7b5bfce90e7c0704891b97d
Vince Gonzalez <Seventito777@gmail.com>
2023-08-09 00:26:45 -0700

    allowing beans to speak anywhere

--- src/server/discord_client.js
+++ src/server/discord_client.js
@@ -93,7 +93,7 @@ discord_client.on('messageCreate', async msg => {
 				}
 			}
 
-			if (run_type == 'production' && msg.channel.name.includes('command') && (msg.content.toLowerCase().startsWith('dahlia') || msg.content.toLowerCase().startsWith('beans') || msg.mentions.users.has(discord_client.user.id) || reply)) {
+			if (run_type == 'production' && (msg.content.toLowerCase().startsWith('dahlia') || msg.content.toLowerCase().startsWith('beans') || msg.mentions.users.has(discord_client.user.id) || reply)) {
 				await require('../utility/openai').distributor(discord_client, msg);
 			}
 		}
