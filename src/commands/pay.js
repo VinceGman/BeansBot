b4d4bbdd0d3353f2d8ca603b15ebcf0f7ee28b11
Vince Gonzalez <Seventito777@gmail.com>
2023-12-12 15:26:58 -0700

    removed discriminator from output

--- src/commands/pay.js
+++ src/commands/pay.js
@@ -15,7 +15,7 @@ module.exports = {
                 .setColor('#000000')
                 .addFields({ name: '+pay 1000 @Sore', value: `pays 1000 credits to Sore.`, inline: false })
                 .addFields({ name: '+pay @Sore 1000', value: `pays 1000 credits to Sore.`, inline: false })
-                .setFooter({ text: `${msg.author.username}#${msg.author.discriminator}` })
+                .setFooter({ text: `${msg.author.username}` })
                 .setTimestamp();
 
             msg.channel.send({ embeds: [pay_guide] });
@@ -25,17 +25,17 @@ module.exports = {
         if (!require('../utility/timers').timer(msg, this.name, this.cooldown)) return; // timers manager checks cooldown
 
         if (msg.mentions.users.size != 1) {
-            msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - **+pay** for more info.`);
+            msg.channel.send(`${msg.author.username} - **+pay** for more info.`);
             return;
         }
 
         let recipient = msg.mentions.users.keys().next().value;
         if (recipient == msg.author.id) {
-            msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - All you do is take and take... **+pay** for more info.`);
+            msg.channel.send(`${msg.author.username} - All you do is take and take... **+pay** for more info.`);
             return;
         }
         if (recipient == discord_client.user.id) {
-            msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - This isn't how you pay taxes... **+pay** for more info.`);
+            msg.channel.send(`${msg.author.username} - This isn't how you pay taxes... **+pay** for more info.`);
             return;
         }
 
@@ -43,16 +43,16 @@ module.exports = {
 
         if (args.length == 1 && !isNaN(args[0])) {
             if (+args[0] < 0) {
-                msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - Your number must be positive.`);
+                msg.channel.send(`${msg.author.username} - Your number must be positive.`);
                 return;
             }
             if (args[0].includes('.')) {
-                msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - Your number must be whole.`);
+                msg.channel.send(`${msg.author.username} - Your number must be whole.`);
                 return;
             }
             if (!(await require('../utility/credits').transaction(discord_client, msg, +args[0]))) return; // credits manager validates transaction
             await require('../utility/credits').refund(discord_client, recipient, +args[0]); // credits manager refunds credits
-            // msg.channel.send(`${msg.author.username}#${msg.author.discriminator} - User paid.`);
+            // msg.channel.send(`${msg.author.username} - User paid.`);
             
             let pay_result = new EmbedBuilder()
                 .setTitle(`Payment Details`)
@@ -61,7 +61,7 @@ module.exports = {
                 // .addFields({ name: 'From', value: `${msg.author.username}`, inline: true })
                 .addFields({ name: `Recipient`, value: `${(await msg.guild.members.fetch(recipient)).user.username}`, inline: true })
                 .addFields({ name: `Amount`, value: `+${+args[0]}`, inline: true })
-                .setFooter({ text: `${msg.author.username}#${msg.author.discriminator}` })
+                .setFooter({ text: `${msg.author.username}` })
                 .setTimestamp();
             msg.channel.send({ embeds: [pay_result] });
             return;
