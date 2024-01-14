3161fa371a72ff1b3d4aa346df07a5092432d54c
Vince Gonzalez <Seventito777@gmail.com>
2024-01-14 13:40:20 -0700

    added booster income to counting

--- src/utility/channels.js
+++ src/utility/channels.js
@@ -3,7 +3,7 @@ module.exports = {
     async channel_function(discord_client, msg, run_type) {
         try {
             switch (msg.channel.id) {
-                case '1184186351260794880': // counting
+                case '1184186351260794880': // main-server // counting
                     if (run_type != 'production') return;
                     await new Promise(r => setTimeout(r, 3000));
 
@@ -17,7 +17,8 @@ module.exports = {
                     if (!count_verification) return;
 
                     let number = msg.content.split(' ').length > 1 ? +msg.content.split(' ')[0] : +msg.content;
-                    if (!isNaN(number)) await require('../utility/credits').refund(discord_client, msg.author.id, number); // credits manager refunds credits
+                    let booster_bonus = msg.member.roles.cache.some(role => role.name.toLowerCase().includes('booster')) ? 1.5 : 1;
+                    if (!isNaN(number)) await require('../utility/credits').refund(discord_client, msg.author.id, number * booster_bonus); // credits manager refunds credits
 
                     break;
             }
