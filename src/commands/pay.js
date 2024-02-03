7c9a0036b8eaec31f6fed1267e5f98e09d5f226c
Vince Gonzalez <Seventito777@gmail.com>
2024-02-03 15:15:48 -0700

    added pay all

--- src/commands/pay.js
+++ src/commands/pay.js
@@ -1,3 +1,6 @@
+const { EmbedBuilder } = require('discord.js');
+const comma_adder = require('commas');
+
 module.exports = {
     name: 'pay',
     description: "pay someone credits",
@@ -6,26 +9,15 @@ module.exports = {
     type: "production",
     cooldown: 6,
     async execute(discord_client, msg, args, admin) {
-        const { EmbedBuilder } = require('discord.js');
-
         if (args.length == 0) {
-            let pay_guide = new EmbedBuilder()
-                .setTitle(`Pay Guide`)
-                .setDescription(`You can pay someone credits. You must mention the user you'd like to pay anywhere in the message.`)
-                .setColor('#000000')
-                .addFields({ name: '+pay 1000 @Sore', value: `pays 1000 credits to Sore.`, inline: false })
-                .addFields({ name: '+pay @Sore 1000', value: `pays 1000 credits to Sore.`, inline: false })
-                .setFooter({ text: `${msg.author.username}` })
-                .setTimestamp();
-
-            msg.channel.send({ embeds: [pay_guide] });
+            this.pay_guide(msg);
             return;
         }
 
         if (!require('../utility/timers').timer(msg, this.name, this.cooldown)) return; // timers manager checks cooldown
 
         if (msg.mentions.users.size != 1) {
-            msg.channel.send(`${msg.author.username} - **+pay** for more info.`);
+            this.pay_guide(msg);
             return;
         }
 
@@ -41,30 +33,55 @@ module.exports = {
 
         args = args.filter(a => !a.includes('<@'));
 
-        if (args.length == 1 && !isNaN(args[0])) {
-            if (+args[0] < 0) {
-                msg.channel.send(`${msg.author.username} - Your number must be positive.`);
-                return;
+        let amount = 0;
+        if (args.length == 1) {
+            if (!isNaN(args[0])) {
+                if (+args[0] < 0) {
+                    msg.channel.send(`${msg.author.username} - Your number must be positive.`);
+                    return;
+                }
+                if (args[0].includes('.')) {
+                    msg.channel.send(`${msg.author.username} - Your number must be whole.`);
+                    return;
+                }
+                amount = +args[0];
             }
-            if (args[0].includes('.')) {
-                msg.channel.send(`${msg.author.username} - Your number must be whole.`);
-                return;
+            else if (args[0].toLowerCase() == 'all') {
+                amount = +(await require('../utility/queries').user(msg.author.id)).credits;
             }
-            if (!(await require('../utility/credits').transaction(discord_client, msg, +args[0]))) return; // credits manager validates transaction
-            await require('../utility/credits').refund(discord_client, recipient, +args[0]); // credits manager refunds credits
-            // msg.channel.send(`${msg.author.username} - User paid.`);
-            
-            let pay_result = new EmbedBuilder()
-                .setTitle(`Payment Details`)
-                .setColor('#37914f')
-                // .setDescription(`Paid: ${+args[0]}`)
-                // .addFields({ name: 'From', value: `${msg.author.username}`, inline: true })
-                .addFields({ name: `Recipient`, value: `${(await msg.guild.members.fetch(recipient)).user.username}`, inline: true })
-                .addFields({ name: `Amount`, value: `+${+args[0]}`, inline: true })
-                .setFooter({ text: `${msg.author.username}` })
-                .setTimestamp();
-            msg.channel.send({ embeds: [pay_result] });
+        }
+        else {
+            this.pay_guide(msg);
             return;
         }
+
+        if (!(await require('../utility/credits').transaction(discord_client, msg, amount))) return; // credits manager validates transaction
+        await require('../utility/credits').refund(discord_client, recipient, amount); // credits manager refunds credits
+        // msg.channel.send(`${msg.author.username} - User paid.`);
+
+        let pay_result = new EmbedBuilder()
+            .setTitle(`Payment Details`)
+            .setColor('#37914f')
+            // .setDescription(`Paid: ${+args[0]}`)
+            // .addFields({ name: 'From', value: `${msg.author.username}`, inline: true })
+            .addFields({ name: `Recipient`, value: `${(await msg.guild.members.fetch(recipient)).user.username}`, inline: true })
+            .addFields({ name: `Amount`, value: `+${comma_adder.add(Math.trunc(amount))} credits`, inline: true })
+            .setFooter({ text: `${msg.author.username}` })
+            .setTimestamp();
+        msg.channel.send({ embeds: [pay_result] });
+        return;
+
+    },
+    pay_guide(msg) {
+        let pay_guide = new EmbedBuilder()
+            .setTitle(`Pay Guide`)
+            .setDescription(`You can pay someone credits. You must mention the user you'd like to pay anywhere in the message.`)
+            .setColor('#000000')
+            .addFields({ name: '+pay 1000 @Sore', value: `pays 1000 credits to Sore.`, inline: false })
+            .addFields({ name: '+pay @Sore 1000', value: `pays 1000 credits to Sore.`, inline: false })
+            .setFooter({ text: `${msg.author.username}` })
+            .setTimestamp();
+
+        msg.channel.send({ embeds: [pay_guide] });
     }
 }
