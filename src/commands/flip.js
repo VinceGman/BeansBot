e95dc813e0142bab44b0c2a6606c841ad9b78bfb
Vince Gonzalez <Seventito777@gmail.com>
2024-02-03 22:28:22 -0700

    stats and streamline update flip

--- src/commands/flip.js
+++ src/commands/flip.js
@@ -1,3 +1,13 @@
+// dashboard: https://console.cloud.google.com/firestore/data?project=beans-326017
+const { Firestore, FieldValue, addDoc } = require('@google-cloud/firestore');
+const db = new Firestore({
+    projectId: 'beans-326017',
+    keyFilename: './service-account.json'
+});
+
+const { EmbedBuilder } = require('discord.js');
+const comma_adder = require('commas');
+
 module.exports = {
     name: 'flip',
     description: "play coin flip",
@@ -8,6 +18,28 @@ module.exports = {
     async execute(discord_client, msg, args, admin) {
         try {
             if (!require('../utility/timers').timer(msg, this.name, this.cooldown)) return; // timers manager checks cooldown
+
+            if (args.length == 1 && args[0].toLowerCase() == 'stats') {
+                let db_user = await require('../utility/queries').user(msg.author.id);
+
+                let flip_stats = new EmbedBuilder()
+                    .setTitle(`Flip Stats`)
+                    .setColor('#000000')
+                    .setFooter({ text: `${msg.author.username}` })
+                    .setTimestamp();
+
+                if (db_user?.times_played_flip && db_user?.times_won_flip && db_user?.net_winnings_flip) {
+                    flip_stats.addFields({ name: 'Winrate', value: `Has won ${(db_user.times_won_flip / db_user.times_played_flip * 100).toFixed(2)}% of ${db_user.times_played_flip} flips.`, inline: false });
+                    let net_credits = db_user.net_winnings_flip >= 0 ? `You've earned ${comma_adder.add(Math.trunc(db_user.net_winnings_flip))} credits.` : `You've lost ${comma_adder.add(Math.trunc(db_user.net_winnings_flip))} credits.`;
+                    flip_stats.addFields({ name: 'Net Credits', value: `${net_credits}`, inline: false });
+                }
+                else {
+                    flip_stats.addFields({ name: 'No Flips', value: 'Play flip to see stats.', inline: false });
+                }
+
+                msg.channel.send({ embeds: [flip_stats] });
+                return;
+            }
 
             let bet = 1000;
             if (args.length == 1 && !isNaN(args[0])) {
@@ -29,16 +61,30 @@ module.exports = {
 
             let max_num = 20;
             let roll_num = Math.floor(Math.random() * max_num) + 1; // [1, 20]
-            let winnings = 0;
-            let outcome = 'Lost';
+            let winnings = roll_num <= max_num / 2 ? bet * 2 : 0;
+            let outcome = winnings > 0 ? 'Won' : 'Lost';
+            let win = roll_num <= max_num / 2 ? 1 : 0;
 
-            if (roll_num <= max_num / 2) {
-                winnings = bet * 2;
-                outcome = winnings > 0 ? 'Won' : 'Lost';
-                await require('../utility/credits').refund(discord_client, msg.author.id, winnings);
+            if (bet >= 1000) {
+                let db_user = await require('../utility/queries').user(msg.author.id);
+                let credits = +db_user.credits;
+                let times_played_flip = db_user?.times_played_flip ? +db_user.times_played_flip : 0;
+                let times_won_flip = db_user?.times_won_flip ? +db_user.times_won_flip : 0;
+                let net_winnings_flip = db_user?.net_winnings_flip ? +db_user.net_winnings_flip : 0;
+
+                credits += winnings;
+                times_played_flip += 1;
+                times_won_flip += win;
+                net_winnings_flip = win == 1 ? net_winnings_flip + bet : net_winnings_flip - bet;
+
+                await db.doc(`members/${msg.author.id}`).set({
+                    credits: credits.toFixed(2).toString(),
+                    times_played_flip: times_played_flip.toString(),
+                    times_won_flip: times_won_flip.toString(),
+                    net_winnings_flip: net_winnings_flip.toFixed(2).toString(),
+                }, { merge: true });
             }
 
-            const { EmbedBuilder } = require('discord.js');
             let flip_embed = new EmbedBuilder()
                 .setTitle(`Coin Flip`)
                 .setColor('#000000')
