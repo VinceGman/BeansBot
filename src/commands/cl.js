db56c9f56cd31e6527f45c8d4e1757ab395dd7ed
Vince Gonzalez <Seventito777@gmail.com>
2024-02-04 01:44:52 -0700

    updated credit leaderboard

--- src/commands/cl.js
+++ src/commands/cl.js
@@ -16,10 +16,12 @@ module.exports = {
     category: 'utility',
     admin: false,
     type: "production",
-    cooldown: 6,
+    cooldown: 60,
     async execute(discord_client, msg, args, admin) {
         try {
             if (!require('../utility/timers').timer(msg, this.name, this.cooldown)) return; // timers manager checks cooldown
+
+            let warning = await msg.reply('Collecting the net worth of all users. Please wait, it may take a moment.');
 
             let leaderboard_embed = new EmbedBuilder()
                 .setTitle('Credits Leaderboard')
@@ -30,9 +32,10 @@ module.exports = {
             let discord_users_ids = [...(await msg.guild.members.fetch()).keys()];
 
             let map_users_ids_credits = new Map();
-            ((await db.collection(`members`).get())._docs()).forEach(async (doc) => {
-                map_users_ids_credits.set(doc._ref._path.segments[1], (+doc.data().credits ?? 0) + +(await require('./credits').get_stocks_value(doc._ref._path.segments[1])));
-            });
+            for (let doc of (await db.collection(`members`).get())._docs()) {
+                let net_credits = (+doc.data().credits ?? 0) + +(await require('./credits').get_stocks_value(doc._ref._path.segments[1]));
+                map_users_ids_credits.set(doc._ref._path.segments[1], net_credits);
+            }
 
             let users_credits = [];
             for (let user_id of discord_users_ids) {
@@ -45,14 +48,15 @@ module.exports = {
 
             for (let user of users_credits) {
                 let discord_user = await msg.guild.members.fetch(user.id);
-                leaderboard_embed.addFields({ name: discord_user.user.globalName, value: `${comma_adder.add(Math.trunc(user.credits))} credits`, inline: false });
+                leaderboard_embed.addFields({ name: discord_user.user.globalName ?? discord_user.user.username, value: `${comma_adder.add(Math.trunc(user.credits))} credits`, inline: false });
             }
 
-            msg.channel.send({ embeds: [leaderboard_embed] });
+            if (warning) warning.delete();
+            msg.reply({ embeds: [leaderboard_embed] });
 
         }
         catch (err) {
-            msg.channel.send('Fetching the leaderboard did not work.');
+            msg.reply('Fetching the leaderboard did not work.');
         }
     }
 }
