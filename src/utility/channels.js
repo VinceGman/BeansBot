ee76e60f266d888204cfa7db44e314c63095e5aa
Vince Gonzalez <Seventito777@gmail.com>
2024-02-18 22:14:41 -0700

    added a special day for extra counting income

--- src/utility/channels.js
+++ src/utility/channels.js
@@ -5,6 +5,8 @@ module.exports = {
             switch (msg.channel.id) {
                 case '1184186351260794880': // main-server // counting
                     if (run_type != 'production') return;
+
+                    let date_bonus = (new Date()).getDate() == 29 || (new Date()).getDate() == 1 ? 2 : 1;
                     await new Promise(r => setTimeout(r, 3000));
 
                     let count_verification = false;
@@ -24,8 +26,7 @@ module.exports = {
 
                     let number = msg.content.split(' ').length > 1 ? +msg.content.split(' ')[0] : +msg.content;
                     let booster_bonus = msg.member.roles.cache.some(role => role.name.toLowerCase().includes('booster')) ? 1.5 : 1;
-                    if (!isNaN(number)) await require('../utility/credits').refund(discord_client, msg.author.id, number * booster_bonus); // credits manager refunds credits
-
+                    if (!isNaN(number)) await require('../utility/credits').refund(discord_client, msg.author.id, number * booster_bonus * date_bonus); // credits manager refunds credits
                     break;
             }
         }
