d6a9a4fb536a20806c9df658f8b6c5499e9eee31
Vince Gonzalez <Seventito777@gmail.com>
2024-03-15 21:49:56 -0700

    price increase

--- src/commands/capital.js
+++ src/commands/capital.js
@@ -167,7 +167,7 @@ module.exports = {
         let solution = '';
 
         let problem_length = Math.floor(capital_income / 192000) + 5;
-        let pay = (problem_length * 200).toString();
+        let pay = (problem_length * 250).toString();
 
         let numbers_range = Math.min((Math.floor(capital_income / 48000) + 3), 9);
         let letters_range = Math.min((Math.floor(capital_income / 48000) + 3), 26);
@@ -193,7 +193,7 @@ module.exports = {
 
         let solution = generate({ minLength: word_length, maxLength: word_length });
         let puzzle = require('lodash').shuffle(solution).join('');
-        let pay = (puzzle.length * 200).toString();
+        let pay = (puzzle.length * 250).toString();
 
         return { puzzle: puzzle, solution: solution, type: type, pay: pay };
     }
