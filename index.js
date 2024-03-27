c7dace414be4ae4f49474ee9a51525e300ab94ee
Vince Gonzalez <Seventito777@gmail.com>
2024-03-27 14:28:02 -0700

    redirect http to https

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ require('./src/utility/setup').setup();
 var express = require('express');
 var app = express();
 
+// Redirect HTTP to HTTPS
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
+
 app.set('view engine', 'ejs');
 
 app.use(require('./src/server/routes'));
