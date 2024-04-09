01a9cc34ba82708d003ad6bc6694a060dbc4e392
Vince Gonzalez <Seventito777@gmail.com>
2024-04-09 01:45:40 -0700

    update vault
    doubled hit rate, added income tracking, removed cooldown for resource locking

--- src/commands/vault.js
+++ src/commands/vault.js
@@ -1,3 +1,10 @@
+// dashboard: https://console.cloud.google.com/firestore/data?project=beans-326017
+const { Firestore } = require('@google-cloud/firestore');
+const db = new Firestore({
+    projectId: 'beans-326017',
+    keyFilename: './service-account.json'
+}); // firestore credentials
+
 const comma_adder = require('commas');
 const { EmbedBuilder } = require('discord.js');
 const { sha256 } = require('../utility/hash')
@@ -15,7 +22,7 @@ module.exports = {
     async execute(discord_client, msg, args, admin) {
         if (!require('../utility/timers').timer(msg, this.name, this.cooldown)) return; // timers manager checks cooldown
 
-        if (vault_codes.length == 0) await this.create_codes(100);
+        if (vault_codes.length == 0) await this.create_codes(200);
 
         if (args.length == 0) {
             let pages = [];
@@ -27,7 +34,7 @@ module.exports = {
                 if (codes_list.length > 900 || ((i + 1) % 20 == 0)) {
                     let hash_guide = new EmbedBuilder()
                         .setTitle(`Vault Guide`)
-                        .setDescription(`Everything you type will generate a unique hash. You're paid if you match a vault code. Each vault code pays 100,000 credits.`)
+                        .setDescription(`Everything you type will generate a unique hash. You're paid if you match a vault code. Each vault code pays 50,000 credits.`)
                         .setColor('#37914f')
                         .addFields({ name: '+vault <input>', value: `You're paid for vault codes that match the hashed input.`, inline: false })
                         .addFields({ name: `Codes - ${page_num}`, value: `${codes_list}`, inline: false })
@@ -41,6 +48,7 @@ module.exports = {
             }
 
             await require('../utility/pagination').paginationEmbed(msg, pages);
+            require('../utility/timers').reset_timer(msg, this.name); // release resource
             return;
         }
 
@@ -59,6 +67,15 @@ module.exports = {
             if (code) {
                 code.claimed = msg.author.username;
 
+                let db_user = await require('../utility/queries').user(msg.author.id);
+                let vault_total = db_user?.vault_total ? +db_user.vault_total : 0;
+
+                vault_total += +code.reward;
+
+                await db.doc(`members/${msg.author.id}`).set({
+                    vault_total: vault_total.toString(),
+                }, { merge: true });
+
                 await require('../utility/credits').refund(discord_client, msg.author.id, +code.reward); // credits manager refunds credits
 
                 hash_output.setColor('#37914f')
@@ -70,6 +87,7 @@ module.exports = {
             }
 
             msg.channel.send({ embeds: [hash_output] });
+            require('../utility/timers').reset_timer(msg, this.name); // release resource
             return;
         }
         catch (err) {
@@ -85,7 +103,7 @@ module.exports = {
             return;
         }
     },
-    async create_codes(count, reward = '100000') {
+    async create_codes(count, reward = '50000') {
         let seed = Math.floor(Date.now() / 1000);
         for (let i = 1; i <= count; i++) {
             vault_codes.push({
