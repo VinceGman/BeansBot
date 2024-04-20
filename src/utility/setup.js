a6e0c3667d2b188328eb2c4d944ceaf609370dae
Vince Gonzalez <Seventito777@gmail.com>
2024-04-20 16:01:04 -0700

    Update setup.js

--- src/utility/setup.js
+++ src/utility/setup.js
@@ -2,6 +2,8 @@ module.exports = {
     async setup() {
         require('dotenv').config(); 
         // configures .env
+
+        process.env.TZ = 'America/New_York'; // sets timezone
         
         require('../server/discord_client'); 
         // initialize discord client
