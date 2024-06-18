5302e504fde900a12c877707f58b672a3ebf0196
Vince Gonzalez <Seventito777@gmail.com>
2024-06-18 00:21:58 -0700

    feat(upgrade.js): add support for upgrading counting bonus to increase counting bonus multiplier by 1x

--- src/commands/upgrade.js
+++ src/commands/upgrade.js
@@ -89,7 +89,7 @@ module.exports = {
                 .setTitle(`Upgrade`)
                 .setDescription(`Increased Wish Chance`)
                 .setColor('#000000')
-                .addFields({ name: 'Wish Chance Multiplier', value: `${((wish_chance - 2)*2)}x -> **${(wish_chance * 2)}x**`, inline: false })
+                .addFields({ name: 'Wish Chance Multiplier', value: `${((wish_chance - 2) * 2)}x -> **${(wish_chance * 2)}x**`, inline: false })
                 .addFields({ name: 'Cost', value: `1,000,000 credits`, inline: false })
                 .setFooter({ text: `${msg.author.username}` })
                 .setTimestamp();
@@ -122,6 +122,31 @@ module.exports = {
             msg.channel.send({ embeds: [upgrade_embed] });
             return;
         }
+        else if (args.join(' ').toLowerCase().includes('counting')) {
+            let cost = 1000000;
+            if (!(await require('../utility/credits').transaction(discord_client, msg, cost))) return; // credits manager validates transaction
+
+            let db_user = await require('../utility/queries').user(msg.guildId, msg.author.id);
+            let counting_upgrade = db_user?.counting_upgrade ? +db_user.counting_upgrade : 1;
+
+            counting_upgrade += 1;
+
+            await db.doc(`servers/${msg.guildId}/members/${msg.author.id}`).set({
+                counting_upgrade: counting_upgrade.toString(),
+            }, { merge: true });
+
+            let upgrade_embed = new EmbedBuilder()
+                .setTitle(`Upgrade`)
+                .setDescription(`Increased Counting Bonus`)
+                .setColor('#000000')
+                .addFields({ name: 'Counting Bonus Multiplier', value: `${counting_upgrade - 1}x -> **${counting_upgrade}x**`, inline: false })
+                .addFields({ name: 'Cost', value: `1,000,000 credits`, inline: false })
+                .setFooter({ text: `${msg.author.username}` })
+                .setTimestamp();
+
+            msg.channel.send({ embeds: [upgrade_embed] });
+            return;
+        }
         else {
             this.upgrade_guide(msg);
             return;
@@ -133,6 +158,7 @@ module.exports = {
         let income_max_charges = db_user?.income_max_charges ? +db_user.income_max_charges : 6;
         let wishlist_max_size = db_user?.wishlist_max_size ? +db_user.wishlist_max_size : 5;
         let wish_chance = db_user?.wish_chance ? +db_user.wish_chance : 5;
+        let counting_upgrade = db_user?.counting_upgrade ? +db_user.counting_upgrade : 1;
 
         let upgrade_guide = new EmbedBuilder()
             .setTitle(`Upgrade Guide`)
@@ -142,6 +168,7 @@ module.exports = {
             .addFields({ name: '+upgrade inventory', value: `Increases your card inventory limit by 100\nCurrent: ${lootbox_total_cards_limit} cards`, inline: false })
             .addFields({ name: '+upgrade wish chance', value: `Increases your wish chance by 4x\nCurrent: ${(wish_chance * 2)}x`, inline: false })
             .addFields({ name: '+upgrade wishlist size', value: `Increases your wishlist size by 1\nCurrent: ${wishlist_max_size} wishes`, inline: false })
+            .addFields({ name: '+upgrade counting', value: `Increases your counting bonus by 1\nCurrent: ${counting_upgrade}x`, inline: false })
             .setFooter({ text: `${msg.author.username}` })
             .setTimestamp();
 
