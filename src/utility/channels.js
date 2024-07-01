ef48f8e6b69edaa1da36d46a929f53d9eeab87bd
Vince Gonzalez <Seventito777@gmail.com>
2024-06-30 21:57:45 -0700

    fix(channels.js): increase date_bonus value to 10 for specific dates to align with requirements feat(channels.js): add support for counting_upgrade feature to adjust credit refund calculation based on user's upgrade level

--- src/utility/channels.js
+++ src/utility/channels.js
@@ -6,7 +6,7 @@ module.exports = {
                 case '1184186351260794880': // main-server // counting
                     if (run_type != 'production') return;
 
-                    let date_bonus = (new Date()).getDate() == 1 || (new Date()).getDate() == 15 ? 5 : 1;
+                    let date_bonus = (new Date()).getDate() == 1 || (new Date()).getDate() == 15 ? 10 : 1;
                     await new Promise(r => setTimeout(r, 3000));
 
                     let count_verification = false;
@@ -26,7 +26,11 @@ module.exports = {
 
                     let number = msg.content.split(' ').length > 1 ? +msg.content.split(' ')[0] : +msg.content;
                     let booster_bonus = msg.member.roles.cache.some(role => role.name.toLowerCase().includes('booster')) ? 1.5 : 1;
-                    if (!isNaN(number)) await require('../utility/credits').refund(discord_client, msg, msg.author.id, number * booster_bonus * date_bonus); // credits manager refunds credits
+                    let db_user = await require('../utility/queries').user(msg.guildId, msg.author.id);
+                    let upgrade = db_user?.counting_upgrade ? db_user.counting_upgrade : 1;
+                    if (!isNaN(number)) await require('../utility/credits').refund(discord_client, msg, msg.author.id, number * booster_bonus * date_bonus * upgrade); // credits manager refunds credits
+
+
                     break;
             }
         }
