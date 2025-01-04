6a54d55c5a183e68258c6ac42bf57c859f639a39
Vince Gonzalez <Seventito777@gmail.com>
2025-01-04 16:56:12 -0700

    Update divinity.js

--- src/commands/divinity.js
+++ src/commands/divinity.js
@@ -338,7 +338,7 @@ module.exports = {
         divinity_embed
             .addFields({ name: `Resources`, value: `\`\`\`Actions: ${actions}\nCoins: ${coins}\`\`\``, inline: false })
             .addFields({ name: `In Play`, value: `\`\`\`${content}\`\`\``, inline: false })
-            .addFields({ name: `Mult`, value: `\`\`\`IP:${(in_play.length > 2 ? ((in_play.length - 2) * 1.5) : 0)} + STATS:${(stats > 8 ? (stats - 8) : 0)} - MAG:${magic} = ${multiplier}x\`\`\``, inline: false })
+            // .addFields({ name: `Mult`, value: `\`\`\`+${(in_play.length > 2 ? ((in_play.length - 2) * 1.5) : 0)}x : In Play\n+${(stats > 8 ? (stats - 8) : 0)}x : Stats\n-${magic}x : Magic\n+${multiplier}x : Multiplier\`\`\``, inline: false }) // (${turns}t)+(${coins}c)+(${actions}a)+(${in_play.length}p)-8
             .setFooter({ text: `${msg.author.username}` })
             .setTimestamp();
 
@@ -396,7 +396,7 @@ module.exports = {
 
         let stats = turns + coins + actions + in_play.length;
         divinity_embed.addFields({ name: `In Play`, value: `\`\`\`${content}\`\`\``, inline: false })
-            .addFields({ name: `Mult`, value: `\`\`\`IP:${(in_play.length > 2 ? ((in_play.length - 2) * 1.5) : 0)} + STATS:${(stats > 8 ? (stats - 8) : 0)} - MAG:${magic} = ${multiplier}x\`\`\``, inline: false })
+            // .addFields({ name: `Mult`, value: `\`\`\`+${(in_play.length > 2 ? ((in_play.length - 2) * 1.5) : 0)}x : In Play\n+${(stats > 8 ? (stats - 8) : 0)}x : Stats\n-${magic}x : Magic\n+${multiplier}x : Multiplier\`\`\``, inline: false }) // (${turns}t)+(${coins}c)+(${actions}a)+(${in_play.length}p)-8
 
         await this.finish_game(msg, outcome, winnings, bet);
 
